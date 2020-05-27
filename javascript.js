d944ecc327131b465ef9eccc29797acbd0409869
katherine Lundy <katherinersl@gmail.com>
2020-05-27 19:51:55 -0400

    added favorites button to functioning button

--- javascript.js
+++ javascript.js
@@ -1,20 +1,26 @@
 // API KEYS ==================================================================
 
-NYTtopStoriesAPIKeyJZ = "RWfhB4SgHGOUTaVyo2DRZJlCqM7fb9iW"
-microsoftAPIKeyJZ = "35cd207dc1msh4b912cdb51003fdp1d33f2jsnedb92f96eb4c"
+var NYTtopStoriesAPIKeyJZ = "RWfhB4SgHGOUTaVyo2DRZJlCqM7fb9iW"
+var microsoftAPIKeyJZ = "35cd207dc1msh4b912cdb51003fdp1d33f2jsnedb92f96eb4c"
 
 //============================================================================
+
+// Global Variables
+var headlines = []
+var favoriteHeadlines = []
+
+$(document).ready(updateFavorites);
 
 // ONCLICK FUNCTION FOR SEARCH
 
 $("#run-search").on("click", function(event){
-    $("#article-section").empty();
     event.preventDefault();
-    
-    topStoriesQueryURL = "https://api.nytimes.com/svc/topstories/v2/"
 
-    articleCount = $("#article-count").val()
-    category = $("#article-category").val() 
+    $("#article-section").empty();
+    var topStoriesQueryURL = "https://api.nytimes.com/svc/topstories/v2/"
+
+    var articleCount = $("#article-count").val()
+    var category = $("#article-category").val() 
 
     topStoriesQueryURL += category + ".json?api-key=" + NYTtopStoriesAPIKeyJZ
     
@@ -24,12 +30,12 @@ $("#run-search").on("click", function(event){
         url: topStoriesQueryURL,
         method: "GET"
     }).then(function(results) {
-    console.log(results)
+        console.log(results)
 
         var articleResults = results.results
         
-        dataString = "{  \"documents\": [{ "
-    
+        var dataString = "{  \"documents\": [{ "
+
         headlines = []
          
         // This for loop does 3 things:
@@ -64,9 +70,9 @@ $("#run-search").on("click", function(event){
             }           
         }  
     
-    // The dataString variable is finished, so let's send it to Microsoft!
+        // The dataString variable is finished, so let's send it to Microsoft!
 
-    callMicrosoftAPI()
+        callMicrosoftAPI(dataString)
 
     })
 })
@@ -84,12 +90,12 @@ $("#clear-all").on("click", function(event) {
 
 // AJAX CALL TO MICROSOFT =======================================================================
 
-function callMicrosoftAPI(){
+function callMicrosoftAPI(dataString){
 
     // This is the object containing all of the information for the AJAX call.
     // Notice how all of that annoying data is contained in that tidy little dataString variable :) 
 
-    microsoftObject = {
+    var microsoftObject = {
         "async": true,
         "crossDomain": true,
         "url": "https://microsoft-azure-text-analytics-v1.p.rapidapi.com/sentiment",
@@ -103,6 +109,7 @@ function callMicrosoftAPI(){
         "processData": false,
         "data": dataString
     }
+
     console.log("dataString = " + dataString)
     console.log(microsoftObject)
 
@@ -116,43 +123,98 @@ function callMicrosoftAPI(){
         
         headlines.sort(function (a, b) {
             return b.score - a.score;
-          });
-          console.log(headlines)
+        });
+          
+        for (k=0; k < headlines.length; k++) {
+            if (headlines[k].score > .5 ) {
+                var newArticleDiv = createHeadlineDiv(headlines[k]);
+                newArticleDiv.appendTo("#positive-articles")
+            }
+            else if(headlines[k].score === .5) {
+                var newArticleDiv = createHeadlineDiv(headlines[k]);
+                newArticleDiv.appendTo("#neutral-articles")
 
-        
-        for (k=0; k < headlines.length; k++){
-        
-        
-          if (headlines[k].score > .5 ) {
-              newArticleDiv = $("<div>")
-              newArticle = $("<a>")
-              newArticle.attr("href", headlines[k].url)
-              newArticle.attr("target", "_blank")
-              newArticle.html(headlines[k].title)
-              newArticle.appendTo(newArticleDiv)
-              newArticleDiv.appendTo("#positive-articles")
-           
-          }
-
-             else if(headlines[k].score === .5) {
-              newArticleDiv = $("<div>")
-              newArticle = $("<a>")
-              newArticle.attr("href", headlines[k].url)
-              newArticle.attr("target", "_blank")
-              newArticle.html(headlines[k].title)
-              newArticle.appendTo(newArticleDiv)
-              newArticleDiv.appendTo("#neutral-articles")
-
-          } else if (headlines[k].score < .5) {
-              newArticleDiv = $("<div>")
-              newArticle = $("<a>")
-              newArticle.attr("href", headlines[k].url)
-              newArticle.attr("target", "_blank")
-              newArticle.html(headlines[k].title)
-              newArticle.appendTo(newArticleDiv)
-              newArticleDiv.prependTo("#negative-articles")
-          }
+            } else if (headlines[k].score < .5) {
+                var newArticleDiv = createHeadlineDiv(headlines[k]);
+                newArticleDiv.prependTo("#negative-articles")
+            }
         }
-    });
-        
+    });    
 }
+
+var star = `
+<svg class="bi bi-star" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+    <path fill-rule="evenodd" d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.523-3.356c.329-.314.158-.888-.283-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767l-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288l1.847-3.658 1.846 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.564.564 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
+</svg>`
+
+function createHeadlineDiv(headline) {
+    var newArticleDiv = $("<li>")
+    var newArticle = $("<a>")
+    
+    // Add favorite button to article div
+    var favButton = $("<button>")
+    favButton.html(star)
+    favButton.addClass("btn mr-2")
+    favButton.attr("data-title", headline.title)
+    favButton.appendTo(newArticleDiv)
+    favButton.click(favoriteButtonClicked)
+    
+    newArticleDiv.addClass("list-group-item")
+    newArticle.attr("href", headline.url)
+    newArticle.attr("target", "_blank")
+    newArticle.html(headline.title)
+    newArticle.appendTo(newArticleDiv)
+    return newArticleDiv
+}
+
+function favoriteButtonClicked(e) {
+    e.preventDefault();
+    console.log("button clicked");
+
+    var headlineTitle = e.currentTarget.dataset.title
+    var inFavorites = false;
+    var headline;
+
+    // Check if already in list
+    for(var i = 0; i < favoriteHeadlines.length; i++) {
+        if(favoriteHeadlines[i].title == headlineTitle) {
+            inFavorites = true
+        }
+    }
+
+    if(!inFavorites) {
+        var headline;
+        for(var i = 0; i < headlines.length; i++) {
+            if(headlines[i].title == headlineTitle) {
+                headline = headlines[i]
+            }
+        }
+       favoriteHeadlines.push(headline)
+    } else {
+        var newFavorites = []
+        for(var i = 0; i < favoriteHeadlines.length; i++) {
+            if(favoriteHeadlines[i].title != headlineTitle) {
+                newFavorites.push(favoriteHeadlines[i])
+            }
+        }
+        favoriteHeadlines = newFavorites
+    }
+
+    localStorage.setItem("favorites", JSON.stringify(favoriteHeadlines))
+
+    updateFavorites()
+}
+
+function updateFavorites() {
+    favoriteHeadlines = JSON.parse(localStorage.getItem("favorites")) || []
+    // Clear section before adding
+    $("#favorite-articles").empty()
+
+    for(var i = 0; i < favoriteHeadlines.length; i++) {
+        var headline = favoriteHeadlines[i];
+        var favArticleDiv = createHeadlineDiv(headline);
+        favArticleDiv.appendTo("#favorite-articles")
+    }
+}
+
+
