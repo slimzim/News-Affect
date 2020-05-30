ffd0f3915d79037600ffb2dfde3ec6da9e75d4e1
kate.m <katiemills0810@gmail.com>
2020-05-30 13:42:27 -0500

    changed dropdown category menu to navbar buttons

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+var d = new Date();
+document.getElementById("date").innerHTML = d;
+
 // API KEYS ==================================================================
 
 var NYTtopStoriesAPIKeyJZ = "RWfhB4SgHGOUTaVyo2DRZJlCqM7fb9iW"
@@ -13,16 +16,18 @@ $(document).ready(updateFavorites);
 
 // ONCLICK FUNCTION FOR SEARCH
 
-$("#run-search").on("click", function(event){
+$(".run-search").on("click", function(event){
     event.preventDefault();
-
+    console.log($(this).val())
     $("#article-section").empty();
     var topStoriesQueryURL = "https://api.nytimes.com/svc/topstories/v2/"
 
     var articleCount = $("#article-count").val()
-    var category = $("#article-category").val() 
+    var category = $(this).val() 
 
     topStoriesQueryURL += category + ".json?api-key=" + NYTtopStoriesAPIKeyJZ
+
+    console.log(topStoriesQueryURL)
     
     // AJAX call to NYT
 
@@ -39,6 +44,8 @@ $("#run-search").on("click", function(event){
         headlines = []
          
         // This for loop does 3 things:
+
+        var articleCount = results.num_results
 
         for (var i=0; i < articleCount; i++) {
 
@@ -143,7 +150,7 @@ function callMicrosoftAPI(dataString){
 }
 
 var star = `
-<svg class="bi bi-star" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+<svg class="bi bi-star" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="https://www.w3.org/2000/svg">
     <path fill-rule="evenodd" d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.523-3.356c.329-.314.158-.888-.283-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767l-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288l1.847-3.658 1.846 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.564.564 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
 </svg>`
 
