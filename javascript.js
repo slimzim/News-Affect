82e1e2b2dc980605cf8faf86ee959a7efbf3f605
slimzim <slimzim@gmail.com>
2020-06-01 12:35:13 -0500

    converted navbar to dropdown, added clear button, all js works, ready for design

--- javascript.js
+++ javascript.js
@@ -1,11 +1,3 @@
-// Today's date (weekday, month, day, year)
-var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-var today  = new Date();
-
-
-document.getElementById("today").innerHTML = today.toLocaleDateString("en-US", options);
-
-
 // API KEYS ==================================================================
 
 var NYTtopStoriesAPIKeyJZ = "RWfhB4SgHGOUTaVyo2DRZJlCqM7fb9iW"
@@ -22,30 +14,19 @@ $(document).ready(updateFavorites);
 
 // ONCLICK FUNCTION FOR SEARCH
 
-$(".run-search").on("click", function(event){
+$("#run-search").on("click", function(event){
     event.preventDefault();
-
-    console.log($(this).val())
-    $("#article-section").empty();
-    var topStoriesQueryURL = "https://api.nytimes.com/svc/topstories/v2/"
-
-    var articleCount = $("#article-count").val()
-    var category = $(this).val() 
-
     var searchTerm = $("#search-term").val().trim()
     console.log("Search Term: " + searchTerm)
 
     $("#article-section").empty();
     var topStoriesQueryURL = "https://api.nytimes.com/svc/topstories/v2/"
-/*
+
     var articleMax = $("#article-max").val();
     console.log(articleMax)
     var category = $("#article-category").val() 
-*/
 
     topStoriesQueryURL += category + ".json?api-key=" + NYTtopStoriesAPIKeyJZ
-
-    console.log(topStoriesQueryURL)
     
     // AJAX call to NYT
 
@@ -63,12 +44,14 @@ $(".run-search").on("click", function(event){
 
         headlines = []
         
+        if (!articleMax) {
+            articleMax = results.num_results
+        }
+    
         // This for loop does 2 things:
 
-        var articleCount = results.num_results
-
         for (var i=0; i < articleCount; i++) {
-
+            
             // 1. Adds the headline to the headlines array, and gives that headline a value.  
            
             var newObject = {
@@ -86,7 +69,6 @@ $(".run-search").on("click", function(event){
             }
 
             else if (searchTerm && i+1 < articleCount && articleResults[i].title.includes(searchTerm) && articleMaxCounter < articleMax) {
-                console.log("Pushing")
                 headlines.push(newObject)
                 articleMaxCounter++
             }
@@ -119,6 +101,10 @@ $("#clear-all").on("click", function(event) {
     $("#article-section").empty();
     $("#search-term").val("");
     searchTerm = ""
+    $("#positive-articles").empty();
+    $("#neutral-articles").empty();
+    $("#negative-articles").empty();
+    $("select").prop('selectedIndex',0)
 })
 
 // ==============================================================================================
@@ -178,7 +164,7 @@ function callMicrosoftAPI(dataString){
 }
 
 var star = `
-<svg class="bi bi-star" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="https://www.w3.org/2000/svg">
+<svg class="bi bi-star" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
     <path fill-rule="evenodd" d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.523-3.356c.329-.314.158-.888-.283-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767l-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288l1.847-3.658 1.846 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.564.564 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
 </svg>`
 
